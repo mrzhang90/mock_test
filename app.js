bde9c6109a35fb2e1e94dac388aac84cc2b31974
张广森 <sp6688@vip.qq.com>
2018-01-19 19:05:48 +0800

    模拟接口数据

--- app.js
+++ app.js
@@ -23,17 +23,82 @@ app.get('/list',function (req, res) {
 	});
 	res.end(output)
 })
+//轮播图
+app.get('/picture_date',function (req, res) {
+	var data = Mock.mock({
+		// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+		'data|10': [{
+				"sort|+1": 1,
+				"image": Mock.Random.image('200x100',Mock.mock('@color'), 'HELLO'),
+				"title":"@cparagraph",
+				"link":"http://www.baidu.com",
+				"datetimeStart":"@datetime('yyyy-MM-dd')",
+				"datetimeEnd":"@datetime('yyyy-MM-dd')",
+				"state|1":[0,1]
+			}]
+	})
+
+	var obj = {
+		"code": 0,
+		"msg": "",
+		"count": 11
+	}
+	res.send(JSON.stringify(Object.assign(data,obj)))
+})
 app.get('/list_date',function (req, res) {
-	res.send(require('./nodeuii/list_date.js'));
+	var data = Mock.mock({
+		// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+		'data|10': [{
+				"sort|+1": 1,
+				"image": Mock.Random.image('200x100',Mock.mock('@color'), 'HELLO'),
+				"title":"@cparagraph",
+				"link":"http://www.baidu.com",
+				"datetimeStart":"@datetime('yyyy-MM-dd')",
+				"datetimeEnd":"@datetime('yyyy-MM-dd')",
+				"game1|1":['浦发北分与中信建投篮球友谊赛','【邀请赛】第五届北京MBA篮球联赛','第五届北京市MBA篮球联赛'],
+				"game2|1":['采购部VS项目联队','总图地质所VS仪控设计所','采购部VS总图地质所'],
+				"state|1":[0,1]
+			}]
+	})
+
+	var obj = {
+		"code": 0,
+		"msg": "",
+		"count": 100
+	}
+	res.send(JSON.stringify(Object.assign(data,obj)))
 })
 app.get('/select_date',function (req, res) {
 	let pid;
+	var json;
 	if(req.originalUrl.indexOf('=')!=-1){
 		pid=req.originalUrl.split('=')[1];
-		res.send(require('./nodeuii/select_date2.js'));
+		json = Mock.mock({
+			// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+			'data|6': [{
+					"Value|+1": 20,
+					"Display|1":['第1一队','第2二队','第3队','第四队','第五队']
+				}]
+		})
 	}else{
-		res.send(require('./nodeuii/select_date.js'));
+		json = Mock.mock({
+			// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+			'data|3': [{
+					"Value|+1": 10,
+					"Display|1":['第一队','第二队','第三队','第四队','第五队']
+				}]
+		})
 	}
+	res.send(json.data)
+})
+app.get('/delete_date',function (req, res) {
+	var data = Mock.mock({'data|1':true })
+	var obj = {
+		"code": 0,
+		"msg": "失败了",
+		"count": 1000
+	}
+	res.send(JSON.stringify(Object.assign(data,obj)))
 })
 //mockJS页面是引用mockJS，路由自己配置，json生成轻松
 app.get('/mockJS',function (req, res) {
